14d555a770ab38520f2660dba8092e63401fc1c4
luffycn <kingla_pei@163.com>
2012-03-15 14:15:41 +0800

    Change the ui and the redirect

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,13 +2,14 @@ class StudentsController < ApplicationController
   respond_to :html, :js
   
   def index
+    @notifications = Notification.all
   end
 
   def create
     Notification.create(
       :user_id => current_user.id,
       :category => 'be_answered',
-      :data => '2'
+      :data => Notification.count + 1
     )
     @notifications = Notification.all
   end
@@ -17,6 +18,8 @@ class StudentsController < ApplicationController
     @notification = Notification.find(params[:id])
     @notification.is_read = true
     @notification.save
+    
+    return redirect_to :back
   end
 
 end
